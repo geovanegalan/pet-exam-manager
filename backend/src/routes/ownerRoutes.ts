0d98253b99952d218d96322f287bf364439f3a6b
Geovane Galan Fiuza <144388433+geovanegalan@users.noreply.github.com>
2026-07-15 19:30:44 -0300

    feat: adiciona rota ownerByCpf

--- backend/src/routes/ownerRoutes.ts
+++ backend/src/routes/ownerRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import {
   createOwner,
+  deleteOwner,
   getAllOwners,
+  getOwnerByCPF,
   getOwnerById,
   updateOwner,
 } from '../services/ownerService';
@@ -11,6 +13,16 @@ const router = Router();
 router.get('/', async (req, res) => {
   try {
     res.json(await getAllOwners());
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get('/cpf/:cpf', async (req, res) => {
+  try {
+    const userCPF = req.params.cpf;
+    const user = await getOwnerByCPF(userCPF);
+    res.json(user);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -42,6 +54,16 @@ router.patch('/:id', async (req, res) => {
     const data = req.body;
     const ownerUpdated = await updateOwner(userId, data);
     res.status(200).json(ownerUpdated);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    await deleteOwner(userId);
+    res.status(200).json({ message: 'Owner deleted successfully' });
   } catch (error) {
     res.status(500).json(error);
   }
